781cf01b694e6dc7cb03a81578014f548a4beb30
Ivan GaLa <github@laodesign.es>
2024-07-27 12:44:33 +0200

    Genero fichero assets para las nubes.

--- src/config/assets/assetsClouds.js
+++ src/config/assets/assetsClouds.js
@@ -2,19 +2,19 @@ const assetsClouds = {
   cloud1: {
     name: 'cloud1',
     asset: 'assets/scenery/overworld/cloud1.png',
-    scale: 0.15
+    setScale: 0.15
   },
 
   cloud2: {
     name: 'cloud2',
     asset: 'assets/scenery/overworld/cloud2.png',
-    scale: 0.15
+    setScale: 0.15
   },
 
   cloud3: {
     name: 'cloud3',
     asset: 'assets/scenery/overworld/cloud3.png',
-    scale: 1
+    setScale: 1
   }
 }
 
